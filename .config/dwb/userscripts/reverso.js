0a6f3ee8aeb37992656553451b29a1afd1436214
Your Name <ciakpey@gmail.com>
2014-07-30 11:47:46 +0200

    ajout script dwb pour reverso

---
+++ .config/dwb/userscripts/reverso.js
@@ -0,0 +1,8 @@
+#!javascript
+
+ 
+bind("ee", function() {
+  execute("tabopen " + "http://dictionnaire.reverso.net/anglais-francais/" + util.getSelection());
+});
+
+// quand je sourligne un passage et que jappui sur ee ça me lance sur reverso 
